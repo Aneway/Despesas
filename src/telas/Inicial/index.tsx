1d68d2bbe99b6c598f0da3df7d7d1a12846db1e3
Aneway <geovanafra27@gmail.com>
2022-09-19 16:53:52 -0300

    first commit

---
+++ src/telas/Inicial/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Inicial(){
+    return(
+        <Conteiner>
+            <Cabecalho>
+
+            </Cabecalho>
+            <Paineis>
+
+            </Paineis>
+            <Transacoes>
+
+            </Transacoes>
+        </Container>
+    )
+}
